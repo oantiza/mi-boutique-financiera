ad78f2499c94a3728e7030b9d27fb8f4bde88b55
Your Name <you@example.com>
2025-12-08 08:03:58 +0100

    Fix: Configure PDFKit as external package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Añade esta línea
+  serverExternalPackages: ['pdfkit'],
 };
 
 export default nextConfig;
